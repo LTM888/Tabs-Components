da50b97606612328af5ca38d2fbd19829a545df4
LTM888 <leigh.tm@gmail.com>
2019-03-14 16:45:13 -0400

    dropdown is done on to the tabs

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,27 +1,30 @@
 class Dropdown {
-  constructor(element) {
+  constructor(dropdown) {
+
+    this.dropdown = dropdown;
     
     // Assign this.element to the dropdown element
-    this.element;
+    
+    
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = this.dropdown.querySelector(".dropdown-button");
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = this.dropdown.querySelector(".dropdown-content");
     
     // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener('click', () => {
-
-    })
+    this.button.addEventListener('click', () => this.linkClick()); 
   }
-
-  toggleContent() {
-    
+  linkClick() {
+      
+  
+      this.content.classList.toggle("dropdown-hidden");
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    // this.content;
   }
 }
+
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
